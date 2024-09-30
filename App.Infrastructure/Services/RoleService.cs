f2212868f05eca21d24e4bd2b8d8a85b33aa34ff
Mamunur Rushid <mamunurrushid60@gmail.com>
2024-09-30 09:26:40 +0600

    Role Create and Get all ok

--- App.Infrastructure/Services/RoleService.cs
+++ App.Infrastructure/Services/RoleService.cs
@@ -1,32 +1,87 @@
-﻿using App.Application.Interfaces;
+﻿using App.Application.Exceptions;
+using App.Application.Interfaces;
+using App.Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 namespace App.Infrastructure.Services
 {
     public class RoleService : IRoleService
     {
-        public Task<bool> CreateRoleAsync(string roleName)
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<ApplicationRole> _roleManager;
+
+        public RoleService(UserManager<ApplicationUser> userManager, RoleManager<ApplicationRole> roleManager)
         {
-            throw new NotImplementedException();
+            _userManager = userManager;
+            _roleManager = roleManager;
         }
 
-        public Task<bool> DeleteRoleAsync(string roleId)
+        public async Task<bool> CreateRoleAsync(string roleName)
         {
-            throw new NotImplementedException();
+            var result = await _roleManager.CreateAsync(new ApplicationRole(roleName));
+            if (!result.Succeeded)
+            {
+                throw new ValidatException(result.Errors);
+            }
+            return result.Succeeded;
         }
 
-        public Task<(string id, string roleName)> GetRoleByIdAsync(string id)
+        public async Task<bool> DeleteRoleAsync(string roleId)
         {
-            throw new NotImplementedException();
+            var roleDetails = await _roleManager.FindByIdAsync(roleId);
+            if (roleDetails == null)
+            {
+                throw new NotFoundException("Role not found");
+            }
+
+            if (roleDetails.Name == "Administrator")
+            {
+                throw new BadRequestException("You can not delete Administrator Role");
+            }
+            var result = await _roleManager.DeleteAsync(roleDetails);
+            if (!result.Succeeded)
+            {
+                throw new ValidatException(result.Errors);
+            }
+            return result.Succeeded;
         }
 
-        public Task<List<(string id, string roleName)>> GetRolesAsync()
+        public async Task<(string id, string roleName)> GetRoleByIdAsync(string id)
         {
-            throw new NotImplementedException();
+            var role = await _roleManager.FindByIdAsync(id);
+            if (role == null)
+            {
+                throw new NotFoundException($"{nameof(role)} does not exist");
+            }
+            return (role.Id, role.Name);
         }
 
-        public Task<bool> UpdateRole(string id, string roleName)
+        public async Task<List<(string id, string roleName)>> GetRolesAsync()
         {
-            throw new NotImplementedException();
+            var roles = await _roleManager.Roles.Select(x => new
+            {
+                x.Id,
+                x.Name
+            }).ToListAsync();
+
+            return roles.Select(role => (role.Id, role.Name)).ToList();
+        }
+
+        public async Task<bool> UpdateRole(string id, string roleName)
+        {
+            if (roleName != null)
+            {
+                var role = await _roleManager.FindByIdAsync(id);
+                if (role == null)
+                {
+                    throw new NotFoundException(" Role does not exist");
+                }
+                role.Name = roleName;
+                var result = await _roleManager.UpdateAsync(role);
+                return result.Succeeded;
+            }
+            return false;
         }
     }
 }
